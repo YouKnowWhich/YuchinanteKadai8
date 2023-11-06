28fe968ffa12d53c8c8d48aa2b3c773cf544c624
yuya-3110 <ysuu45143745@gmail.com>
2023-11-06 22:11:40 +0900

    first commit

---
+++ YuchinanteKadai8/GreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GreenViewController.swift
+//  YuchinanteKadai8
+//  
+//  Created by Yuchinante on 2023/11/06
+//  
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
